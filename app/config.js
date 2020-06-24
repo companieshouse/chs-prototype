e5e96547979fb52efa1b51d88b63ac504ba71cc7
Emma James <ejames@companieshouse.gov.uk>
2020-06-24 10:00:22 +0100

    renaming the service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Enhanced search',
+  serviceName: 'Companies House enhanced search',
 
   // Default port that prototype runs on
   port: '3000',
