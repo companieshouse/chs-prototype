79de006df9982844574f823b7b8b1bb8f80e3cf6
Emma James <ejames@companieshouse.gov.uk>
2021-01-05 11:05:03 +0000

    restoring changes - may need to roll back

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,15 @@
 /* global $ */
 
+
 // Warn about using the kit in production
 if (window.console && window.console.info) {
   window.console.info('GOV.UK Prototype Kit - do not use for production')
 }
+
+
+$(document).ready(function() {
+    $('.location').select2()
+});
 
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
