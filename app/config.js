eed8c64af667318b24ec19a26e0f148ba856ea1b
Emma James <ejames@companieshouse.gov.uk>
2021-01-14 17:09:08 +0000

    removing the table layout of results

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Companies House enhanced search',
+  serviceName: '',
 
   // Default port that prototype runs on
   port: '3000',
