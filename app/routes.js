2e084faa37b15975ef4512368ef01b3a770dfefd
Emma James <ejames@companieshouse.gov.uk>
2021-03-25 14:29:58 +0000

    removing the error message rel 1.2

--- app/routes.js
+++ app/routes.js
@@ -22,17 +22,10 @@ router.post('/r1-2/dissolved-search', function(req, res) {
     selectNameHasError = true;
     errors.push({text: "Select which company names you want to search", href: "#select-name-error"});
   }
-  if(typeof req.session.data['alpha-display'] !== 'undefined' & req.session.data['select-name'] == "previous" ){
+  if(typeof req.session.data['alpha-display'] !== 'undefined'){
+
     previousNameAndAlphaHasError = true;
     errors.push({text: "Select company name when they were dissolved. Previous company names is not currently available for alphabetical search", href: "#previous-name-and-alpha-error"});
-  }
-  if(companyNameHasError | selectNameHasError | previousNameAndAlphaHasError){
-    res.render('dissolved/r1-2/dissolved-search', {
-          errorCompanyName: companyNameHasError,
-          errorSelectName: selectNameHasError,
-          errorPreviousNameAndAlpha: previousNameAndAlphaHasError,
-          errorList: errors
-        })
   }
   else
   {
