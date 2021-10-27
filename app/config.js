bf7bd4c8cc96d584edbd59752038fc8f9efc16c9
Michael Gearon <mgearon@companieshouse.gov.uk>
2021-10-27 10:56:29 +0100

    Changes

--- app/config.js
+++ app/config.js
@@ -7,7 +7,7 @@ module.exports = {
   serviceName: 'Companies House',
 
   // Default port that prototype runs on
-  port: '5555',
+  port: '3000',
 
   // Enable or disable password protection on production
   useAuth: 'true',
@@ -24,9 +24,6 @@ module.exports = {
 
   // Force HTTP to redirect to HTTPS on production
   useHttps: 'true',
-
-  // Cookie warning - update link to service's cookie page.
-  cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#">Find out more about cookies</a>',
 
   // Enable or disable Browser Sync
   useBrowserSync: 'true'
